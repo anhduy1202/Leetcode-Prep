8ac5a5611ef8b647d3579870e2389a598a15c992
danieltruong <aduy1122@gmail.com>
2022-10-31 02:43:53 -0700

    Added necessary Tree problem to prep interview

--- Tree Concept/pathSumII.py
+++ Tree Concept/pathSumII.py
@@ -1,6 +1,6 @@
 from TreeNode import TreeNode
 
-def pathSum(root, targetSum):
+def pathSumRecursive(root, targetSum):
     res = []
 
     def dfs(root, currSum, arr):
@@ -17,6 +17,22 @@ def pathSum(root, targetSum):
     dfs(root, targetSum, [])
     return res
 
+def pathSumIterative(root, targetSum):
+    if not root:
+        return []
+    stack = [(root,targetSum,[root.val])]
+    res = []
+    while stack:
+        node, currSum, path = stack.pop()
+        if not node.left and not node.right:
+            if currSum == node.val:
+                res.append(path)
+        if node.left:
+            stack.append((node.left, currSum - node.val, path+[node.left.val]))
+        if node.right:
+            stack.append((node.right, currSum - node.val, path+[node.right.val]))
+    return res
+
 Node4 = TreeNode(val=4)
 Node5 = TreeNode(val=5)
 Node2 = TreeNode(val=2)
@@ -24,4 +40,4 @@ Node7 = TreeNode(val=7)
 Node7_1 = TreeNode(val=7,left=Node4,right=Node5)
 Node9 = TreeNode(val=9,left=Node2,right=Node7)
 root = TreeNode(val=1, left=Node7_1, right=Node9)
-print(pathSum(root,12))
+print(pathSumRecursive(root,12))
---
+++ Tree Concept/treeAllSum.py
@@ -0,0 +1,52 @@
+'''
+Given a Binary Tree, return all sum from node to leaves
+Ex:
+        5
+    2       1
+ 3     4        6
+
+return [21,9,3,4,7,6]   
+'''
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+def findAllSum(root):
+    if not root:
+        return []
+    stack = [root]
+    res = []
+    cache = {}
+    def dfsSum(root):
+        if not root: 
+            return 0
+        if root.val in cache:
+            return cache[root.val]
+        else:
+            currSum = root.val
+            currSum += dfsSum(root.left)
+            currSum += dfsSum(root.right)
+            cache[root.val] = currSum
+        return currSum
+
+    while stack:
+        node = stack.pop()
+        sum = dfsSum(node)
+        res.append(sum)
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+    return res
+
+NODE3 = TreeNode(val=3)
+NODE4 = TreeNode(val=4)
+NODE6 = TreeNode(val=6)
+NODE1 = TreeNode(val=1, right=NODE6)
+NODE2 = TreeNode(val=2, left=NODE3, right=NODE4)
+root = TreeNode(val=5, left=NODE2, right=NODE1)
+print(findAllSum(root))
