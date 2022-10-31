d36a1d1a3dbe90aec016f01eb394f5fe205c15dd
danieltruong <aduy1122@gmail.com>
2022-10-31 10:49:07 -0700

    treeAllSum

--- Tree Concept/treeAllAverage.py
+++ Tree Concept/treeAllAverage.py
@@ -1,11 +1,11 @@
 '''
-Given a Binary Tree, return all sum from node to leaves
+Given a Binary Tree, return all average from node to leaves
 Ex:
         5
     2       1
  3     4        6
 
-return [21,9,3,4,7,6]   
+return [3.5,3,3,4,3.5,6]   
 '''
 
 class TreeNode:
@@ -15,20 +15,30 @@ class TreeNode:
         self.left = left
         self.right = right
 
-def findAllSum(root):
+def findAllAvg(root):
     if not root:
         return []
     res = []
-    def dfsSum(root):
+
+    def height(root):
+        if not root:
+            return 0
+        left = height(root.left)
+        right = height(root.right)
+        return max(left,right) + 1
+    treeHeight = height(root)
+
+    def dfsAvg(root, count):
         if not root: 
             return 0
         else:
             currSum = root.val
-            currSum += dfsSum(root.left)
-            currSum += dfsSum(root.right)
-            res.append(currSum)
+            currSum += dfsAvg(root.left, count + 1)
+            currSum += dfsAvg(root.right, count + 1)
+            if count > 0:
+                res.append(currSum / (treeHeight - count))
         return currSum
-    dfsSum(root)
+    dfsAvg(root,0)
     return res
 
 NODE3 = TreeNode(val=3)
@@ -37,4 +47,4 @@ NODE6 = TreeNode(val=6)
 NODE1 = TreeNode(val=1, right=NODE6)
 NODE2 = TreeNode(val=2, left=NODE3, right=NODE4)
 root = TreeNode(val=5, left=NODE2, right=NODE1)
-print(findAllSum(root))
+print(findAllAvg(root))
